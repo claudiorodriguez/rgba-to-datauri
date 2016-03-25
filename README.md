1777a287c02b229d4549fa6822367fa6653ba9e7
Claudio Rodriguez <cjrodr@yahoo.com>
2016-03-25 18:03:59 -0300

    Add tests, readme, CI

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # rgba-to-datauri
-Converts a flat array of RGBA quadruplets (e.g. [ [1,2,3,0], [2,3,4,0], ... ] ) into a PNG base64 encoded datauri.
+
+[![Build Status](https://secure.travis-ci.org/claudiorodriguez/rgba-to-datauri.png)](http://travis-ci.org/claudiorodriguez/rgba-to-datauri)
+
+Converts a flat array of RGBA quadruplets (array of 4 integers) into a PNG base64 encoded datauri.
+
+## TODO
+
+* Error handling, input validation.
+* Promisifying (right now `convert` is sync)
+
+## Installation
+
+### node.js
+
+Install using [npm](http://npmjs.org/):
+
+```bash
+$ npm install rgba-to-datauri
+```
+
+## Examples
+
+**Node.JS**
+
+```javascript
+const rgbaToDataUri = require('rgba-to-datauri');
+
+const array = [
+  [0,0,0,255], [0,0,0,255],
+  [0,0,0,255], [0,0,0,255]
+];
+
+// Should get a png base64 encoded datauri depicting a black 2px by 2px square
+let uri = rgbaToDataUri.convert(array, 2, 2);
+```
+
+## Testing
+
+To run the tests:
+
+```bash
+$ npm install
+$ npm test
+```
+
+## Contributing
+
+If you wish to submit a pull request please update and/or create new tests for any changes you make and ensure the tests pass.
+
+## License
+
+MIT - see [LICENSE](https://github.com/claudiorodriguez/rgba-to-datauri/blob/master/LICENSE)
--- test/convert.js
+++ test/convert.js
@@ -1,8 +1,14 @@
 'use strict';
 
-const rgbaToDataUri = require('./index');
+const chai = require('chai'),
+    rgbaToDataUri = require('../index');
 
-const testArray = [
+const expect = chai.expect,
+    assert = chai.assert;
+
+// ----- Basic tests ----- //
+
+const testFlatArray = [
   [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255],
   [0,0,0,255], [255,0,0,255], [255,0,0,255], [255,0,0,255], [255,0,0,255], [255,0,0,255], [255,0,0,255], [0,0,0,255],
   [0,0,0,255], [255,0,0,255], [0,255,0,255], [0,255,0,255], [0,255,0,255], [0,255,0,255], [255,0,0,255], [0,0,0,255],
@@ -13,4 +19,22 @@ const testArray = [
   [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255], [0,0,0,255]
 ];
 
-console.log(rgbaToDataUri.convert(testArray, 8, 8));
+const expectedFlatArrayUri = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAO0lEQVR4AYXB0Q0A' +
+  'EBBEwbdyHem/BDUtER9HhBkB5iEYzJ2AYJHZWEyFTK7IlaTwEWRW4xAsFlcCzEMHq+YKSPX/JJQAAAAASUVORK5CYII=';
+
+
+exports['Convert RGBA to PNG datauri'] = {
+  'from flat array of rgba quads': function(done) {
+    this.timeout(5000);
+
+    let startTime = new Date().valueOf();
+
+    let uri = rgbaToDataUri.convert(testFlatArray, 8, 8)
+
+    expect(uri).to.eql(expectedFlatArrayUri);
+
+    var timeElapsed = new Date().valueOf() - startTime;
+    console.log(timeElapsed + ' ms');
+    done();
+  }
+};
